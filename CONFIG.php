a39f26d8abeb77aa719c5f64504ae5e7042be4c0
Justin Isaf <git@justinisaf.com>
2013-11-11 09:58:04 -0500

    Initial Commit

---
+++ CONFIG.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * DryThemes
+ *
+ * @author		DryThemes
+ * @copyright   Copyright (c) 2012, DryThemes.
+ * @link		www.drythemes.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+/*
+  | -------------------------------------------------------------------
+  | NOVO CONFIG
+  | -------------------------------------------------------------------
+  | Please fill your site configation details here.
+  | CHANGE ONLY THIS FEW LINE BELLOW
+*/
+
+//SITE GLOBAL CONFIGURATION
+$email = "communl@justinisaf.com";   //<-- Your email
+
+//SOCIAL CONFIGURATION
+$twitter_name = "communl";    //<-- Your Twitter Name
+?>
---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Communl - Community Management Services</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="Template by Dry Themes" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, PHP, Minimal Awesomness" />
+        <meta name="author" content="DryThemes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <link rel="shortcut icon" href="images/favicon.ico" />    
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Crete+Round' rel='stylesheet' type='text/css'> 
+        <link type="text/css" rel="stylesheet" href="css/clear.css"/>
+        <link type="text/css" rel="stylesheet" href="css/carouFredSel.css"/>
+        <link type="text/css" rel="stylesheet" href="css/common.css"/>
+        <link type="text/css" rel="stylesheet" href="css/columns.css"/> 
+        <link type="text/css" rel="stylesheet" href="css/columns_with_border.css"/> 
+        <link type="text/css" rel="stylesheet" href="css/prettyPhoto.css"/> 
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <link type="text/css" rel="stylesheet" href="css/colors.css"/>
+        <link type="text/css" rel="stylesheet" href="css/responsive.css"/>
+        <link type="text/css" rel="stylesheet" href="images/font-awesome/font-awesome.min.css">
+
+        <!--[if lt IE 9]>
+                <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+                <script type="text/javascript" src="js/respond.src.js"></script>
+        <![endif]-->
+        
+         <!--Load JavaScript-->
+        <script type="text/javascript" src="js/jquery-1.8.1.min.js"></script>                  
+        <script type="text/javascript" src="js/jquery.sticky.js"></script>                 
+        <script type="text/javascript" src="js/jquery.dryMenu.js"></script> 
+        <script type="text/javascript" src="js/small-menu.js"></script>
+        <script type="text/javascript" src="js/jquery.carouFredSel-6.2.0-packed.js"></script>
+        <script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
+        <script type="text/javascript" src="js/jquery.touchSwipe.min.js"></script>        
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/jquery.myHint.js"></script>  
+        <script type="text/javascript" src="js/portfolio.js"></script>
+        <script type="text/javascript" src="js/jquery.vticker-min.js"></script>
+        <script type="text/javascript" src="php/scriptsData.php"></script>
+        <script type="text/javascript" src="js/jquery.tweets.js"></script>  
+        <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>     
+        <script type="text/javascript" src="js/main.js"></script>
+        
+    </head>
+    <body>  
+        <table class="doc-loader">
+            <tr>
+                <td>
+                    <img src="images/ajax-document-loader.gif" alt="Loading..." />
+                </td>
+            </tr>
+        </table>
+        <!--Navigation Menu-->
+        <div id="main-menu" class="main-menu">      
+            <nav class="center-relative">
+                <ul>
+                    <li class="active"><a href="#home">HOME</a></li>
+                    <li><a href="#services">SERVICES</a></li>
+                    <li><a href="#featured_work">WORK</a></li>
+                    <li><a href="#about">ABOUT</a></li>
+                    <li><a href="#contact">CONTACT</a></li>
+                </ul>
+            </nav>      
+        </div>
+        <!--Home-->
+        <div id="home" class="home section background-fixed slider_holder">
+            <div class="home-title-holder">
+                <div class="logo"><img src="images/bak.png"/>
+                    <p><span class="logo-text">A SMARTER STRUCTURE FOR YOUR COMMUNITY TEAM</span></p> 
+                </div>
+                <div class="block content-960 center-relative">
+            </div>
+                <div class="title-description">
+                    <p>We are a small firm that will help you set your strategy, hire the right front line team to manage day to day, and then stay with you to provide decades of experience as your community grows. </p>
+                </div>
+                <div class="title-button-holder center-text">
+                    <a href="#contact" class="button-border big white slow-scroll">GET IN TOUCH</a>
+                </div>  
+            </div>
+        </div>
+        <!--Services-->
+        <div id="services" class="clear services section center-text block">
+            <h2>Services</h2>
+            <img class="separator_x" src="images/separators/separator_x.png" alt="____" />      
+            <div class="block content-960 center-relative">
+                <p class="info">
+                    Getting a community off the ground needs a solid strategy, needs savvy implementation, and needs to be as inexpensive as possible. Often you can’t have all three, but we can help you get pretty close.
+                    <br/><br/>
+                    We are your in-house community strategy team for when you don't need a full time senior level strategist, freeing your budget to hire the best day-to-day community manager.
+                    <br/><br/>
+                    Get in touch.
+                </p>
+                <section class="one_third">
+                    <img class="block center-relative" src="images/services_icon_02.png" alt="" />                              
+                    <h4>HIGH LEVEL <br/>COMMUNITY STRATEGY</h4>
+                    <p>
+                        We join your team on retainer, bringing decades of experience to help drive strategy, long term vision and ongoing analysis. By letting us manage the high level, you gain significantly more flexibility in the skill level of your full time hire, so you can get going much faster, for a lower cost, and a better output.
+                    </p>
+                </section>
+                <section class="one_third">
+                    <img class="block center-relative" src="images/services_icon_03.png" alt="" />  
+                    <h4>HIRING <br/>SUPPORT</h4>
+                    <p>
+                        We've hired a lot of people in our time. Let us help you define the scope and role of your new community hire, write the job description, and attract the right candidates. We can also help you evaluate the skills and aptitude of candidates you like, ensuring that you get the best fit for what you need.
+                    </p>
+                </section>
+                <section class="one_third last">
+                    <img class="block center-relative" src="images/services_icon_01.png" alt="" />              
+                    <h4>RIGHT-HAND<br/> MEN</h4>
+                    <p>
+                        We've been through it all and come out breathing (and often better off) on the other side. We put that experience to work for you, be it a quick questions or an urgent crisis, we're always right there for you. Our goal is to make you, your community and your team more awesome in everything you do.
+                    </p>
+                </section>
+                <!--clear section-->
+                <div class="clear"></div>                   
+                <div class="separator blog_separator top-40"></div>
+                    <p> Let's grab a coffee and chat about how we can help your community.</p>
+                <a class="button black slow-scroll" href="#contact">CONTACT US</a>   
+            </div>
+            <div class="clear"></div>   
+        </div>
+
+        <!--Featured work-->
+        <div id="featured_work" class="clear featured_work section center-text block">
+            <h2>PREVIOUS WORK</h2>
+            <img class="separator_x" src="images/separators/separator_x.png" alt="____" />
+            <div class="block content-960 center-relative">             
+                <p class="info">
+                    Our team has worked with some of the biggest brands in the business, and many just getting started. We've worked in travel, finance, media, gaming, social activism, user generated content, customer support, and many other industries. This is just a sampling of where we've spent our time.
+                </p>        
+                <div class="separator featured_work_separator"></div>
+                <section class="one_half_with_border text-right">   
+                    <p class="featured-work-section-title crete-round-font bottom-5">The Huffington Post</p>
+                    <p>
+                        Growing the largest actively managed community on the internet by up to 50% month over month, reaching 400,000 <em>pre-moderated</em> comments a day with the most efficient cost-per-comment numbers for machine assisted human moderation in the industry. 
+                    </p>
+                </section>
+                <section class="one_half_with_border last border-left text-left">
+                    <p class="featured-work-section-title crete-round-font bottom-5">Answers.com</p>
+                    <p>
+                        Managing 100,000 user submitted contributions viewed by 50 million site visitors per month and a team of 17 local and remote community and content managers. Setting up systems for content vetting, and community recognition increasing quality and quantity of UGC.
+                    </p>
+                </section>
+                <!--clear section-->
+                <div class="clear"></div>   
+                <div class="separator blank_separator"></div>
+                <section class="one_half_with_border text-right">   
+                    <p class="featured-work-section-title crete-round-font bottom-5">ConquerClub</p>
+                    <p>
+                        Growing the player community of this online game from 300 to 300,000 users and building a volunteer community and UGC team generating over $2 million in labor hours per year across customer support, content creation, tournament management and internationalization.
+                    </p>
+                </section>
+                <section class="one_half_with_border last border-left text-left">
+                    <p class="featured-work-section-title crete-round-font bottom-5">Charles and Helen Schwab Foundation</p>
+                    <p>
+                        Full life-cycle of a community for parents of children with learning disabilities including strategy, reporting, policy and procedure planning, on-going management and product design and implementation for large scale community features for parents and children.
+                    </p>
+                </section>                         
+                <!--clear section-->
+                <div class="clear"></div>                   
+            </div>
+             <div class="full-width top-5">
+                <div class="clear"></div>
+                <div class="service_slider_background background-fixed left">
+                    <div class="service_slider_holder slider_holder">
+                        <ul id="service_text_slider" class="slides center-text crete-round-font">                            
+                            <li>
+                                "Justin is the real deal. He's someone who truly understands what it means to build a community"
+                                <p class="quote-author top-5">David Spinks &nbsp; / &nbsp; Co-Founder, TheCommunityManager.com and LetsFea.st</p>
+                            </li>
+                            <li>
+                                "Working with Scott Moore provided me with a chance to learn about online communities from one of the best."
+                                <p class="quote-author top-5">Todd D. Blank &nbsp; / &nbsp; Mobile Interaction Design Lead, JP Morgan Chase</p>
+                            </li>
+                            <li>
+                                "Justin's incredible passion for online community building is outweighed only by his in-depth knowledge and experience in formulating and implementing community building strategies."
+                                <p class="quote-author top-5">MacGregor Thomson &nbsp; / &nbsp; Product Manager, HuffingtonPost</p>
+                            </li>       
+                        </ul>
+                        <div id="service_text_slide_pager" class="carousel_pagination left"></div>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+
+        <!--About-->
+        <div id="about" class="clear about section center-text block">
+            <h2>ABOUT THE TEAM</h2>
+            <img class="separator_x" src="images/separators/separator_x.png" alt="____" />              
+            <div class="block content-960 center-relative center-text">
+                <p class="info">
+                    We've been planning, building, and fostering communities professionally for more than 25 years combined. We started back when dial-up was the norm, Geocities was popular, and when GIFs were cool the first time.
+                    <br/><br/>
+                    We absolutely love what we do.
+                </p>
+                <div class="separator about_separator"></div>
+                <div class="one_half_with_border text-center top-10">
+                    <div class="team-item-holder">
+                        <div class="team border-200">
+                            <div class="white-border relative border-200">
+                                <img class="team-image border-200" src="images/Justin.png" alt="" />
+                                <span class="shadow border-200"></span>
+                            </div>
+                        </div>
+                        <h4>JUSTIN ISAF</h4>
+                        <div class="team-position">CO-FOUNDER</div>  
+                        <div class="team-social">
+                            <a href="http://www.twitter.com/justinisaf"><img src="images/twitter_icon.png" alt="" /></a>
+                            <a href="http://www.linkedin.com/in/communitymanagement"><img src="images/linkedin_icon.png" alt="" /></a>
+                            </div>
+                        <div class="team-content top-50">
+                            Justin specialized in communities at scale, moderation, user generated content and combattive communities (the ones where people fight a lot). He splits his time between New York and DC.
+                        </div>  
+                    </div>
+                </div>
+                <div class="one_half_with_border last border-left text-center top-10">
+                    <div class="team-item-holder">
+                        <div class="team border-200">
+                            <div class="white-border relative border-200">
+                                <img class="team-image border-200" src="images/Scott.jpg" alt="" />
+                                <span class="shadow border-200"></span>
+                            </div>
+                        </div>
+                        <h4>SCOTT MOORE</h4>
+                        <div class="team-position">CO-FOUNDER</div>  
+                        <div class="team-social">
+                            <a href="https://twitter.com/scottmoore"><img src="images/twitter_icon.png" alt="" /></a>
+                            <a href="http://www.linkedin.com/in/scottmoore"><img src="images/linkedin_icon.png" alt="" /></a>
+                        </div>
+                        <div class="team-content top-50">
+                            Scott specializes in starting and transitioning communities, for-profit and non-profit community strategy, volunteer management, and virtual worlds. He works in the San Francisco Bay Area.
+                        </div>  
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div> 
+            <div class="clear"></div>
+        </div>
+
+        <!--Contact-->
+        <div id="contact" class="clear contact section center-text block">
+            <h2>CONTACT</h2>
+            <img class="separator_x" src="images/separators/separator_x.png" alt="____" />              
+            <div class="block content-960 center-relative center-text">
+                <p class="info">
+                    We work in person on both the Left Coast and the East Coast, or remotely with you anywhere in the world. Get in touch with us and we'd love to start discussing what you're doing and how we might be able to help.
+                </p>
+                <div class="separator contact_separator"></div>         
+                <section class="block content-960 center-relative center-text">                  
+                    <div class="contact-form">                
+                        <ul id="contact-form">
+                            <li class="name-holder">
+                                <input type="text" id="name" />
+                            </li>
+                            <li class="email-holder">
+                                <input type="text" id="contact-email" />
+                            </li>
+                            <li class="message-holder">
+                                <textarea rows="7" cols="50" id="message"></textarea>
+                            </li>
+                            <li class="last-child">
+                                <a class="button black" onClick="SendMail()" id="send-message">SUBMIT</a>   
+                            </li>
+                        </ul>
+                        <div class="clear"></div>                   
+                    </div>
+                    <div class="clear"></div>                   
+                    <div class="sending-message-status-holder"><span id="sending-message-status">&nbsp;</span></div>
+                </section>
+                <div class="clear"></div>
+            </div>  
+        </div>
+
+        <!--Footer-->
+        <footer class="clear center-text">
+            <div class="footer-text bottom-50 crete-round-font">
+                Ohai! You made it to the bottom. You're awesome! Let us know you saw this and we'll buy you coffee just because :)
+            </div>
+            <!--
+            <div class="footer-social">
+                <a href="#"><img src="images/facebook_footer.png" alt="" /></a>
+                <a href="#"><img src="images/twitter_footer.png" alt="" /></a>
+                <a href="#"><img src="images/pinterest_footer.png" alt="" /></a>
+                <a href="#"><img src="images/dribbble_footer.png" alt="" /></a>
+                <a href="#"><img src="images/linkedin_footer.png" alt="" /></a>     
+            </div>
+            -->
+            <div class="footer-copyright top-80">
+                &copy; Communl
+            </div>
+        </footer>  
+        <!--CrazyEgg Code -->
+        <script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0015/1440.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
+        
+    </body>
+</html>
